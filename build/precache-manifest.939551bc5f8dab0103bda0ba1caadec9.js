9989cba905b667a50d30678c166d5d543923a2ff
Shikhar99 <me.shikhar99@gmail.com>
2021-02-16 12:13:36 +0530

    initial commit

---
+++ build/precache-manifest.939551bc5f8dab0103bda0ba1caadec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b393eedaa7682da792dc4e2fe351cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140fc5ee5f775ed46617",
+    "url": "/static/css/main.00cf29bb.chunk.css"
+  },
+  {
+    "revision": "8d42b92a34055e108536",
+    "url": "/static/js/2.61997b1b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.61997b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140fc5ee5f775ed46617",
+    "url": "/static/js/main.294bef67.chunk.js"
+  },
+  {
+    "revision": "2b55614ca07f93359093",
+    "url": "/static/js/runtime-main.bfd8333c.js"
+  }
+]);
